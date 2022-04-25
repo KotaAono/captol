52e6cdac325dee9c87b4581aa35a3e572d3a465a
KotaAono <aono@hnl.t.u-tokyo.ac.jp>
2022-04-25 11:43:58 +0900

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="captol",
+    version="2.0",
+    license="MIT",
+    description="A python-based GUI application for reconstructing screen-sharing documents.",
+    packages=find_packages()
+)
