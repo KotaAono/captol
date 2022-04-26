8ef2e01f4bed6e26dad07b006d89def4c5a3325b
KotaAono <aono@hnl.t.u-tokyo.ac.jp>
2022-04-26 20:54:04 +0900

    Update

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     version="2.0",
     license="MIT",
     description="A python-based GUI application for reconstructing screen-sharing documents.",
-    packages=find_packages()
+    packages=["captol"]
 )
