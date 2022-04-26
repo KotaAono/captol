53f440fb9c8c23b9630b6d1eabdfb09167ca0f54
KotaAono <aono@hnl.t.u-tokyo.ac.jp>
2022-04-26 17:14:54 +0900

    Modify path operation

--- captol/utils/const.py
+++ captol/utils/const.py
@@ -1,5 +1,5 @@
-from os.path import join, dirname
+from os.path import join, dirname, abspath
 
-ENV_FILE = join(dirname( __file__), '../cache/env.json')
-ICON_FILE = join(dirname(__file__), '../icon/icon.ico')
-AREA_FILE = join(dirname(__file__), "../cache/areas.json")
+ENV_FILE = abspath(join(dirname( __file__), '..', 'cache', 'env.json'))
+ICON_FILE = abspath(join(dirname(__file__), '..', 'icon', 'icon.ico'))
+AREA_FILE = abspath(join(dirname(__file__), '..', 'cache', 'areas.json'))
