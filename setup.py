367db4e31b455ea7ec06d619227639794b3fc288
KotaAono <aono@hnl.t.u-tokyo.ac.jp>
2022-05-02 16:05:20 +0900

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="captol",
-    version="2.1",
+    version="2.2",
     license="MIT",
     description="A python-based GUI application for reconstructing screen-sharing documents.",
     packages=find_packages(),
